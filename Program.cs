a09182a7e3bb02e209efc8e04ee83d270c431561
20100 <mmmelkady23@gmail.com>
2025-01-18 19:21:47 +0200

    All Assignment

--- Program.cs
+++ Program.cs
@@ -1,10 +1,115 @@
-﻿namespace ConsoleApp1
+﻿using ConsoleApp1.NewFolder;
+using ConsoleApp1.Second;
+using ConsoleApp1.Third_project;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+            #region First Project 
+            ////_3DPonit P = new _3DPonit(10, 20, 30);
+            ////Console.WriteLine(P.ToString());
+
+            //Console.WriteLine("Enter x , y, z for point one");
+            //string P1_Input = Console.ReadLine();
+            //Console.WriteLine("Enter x , y, z for point Two");
+            //string P2_Input = Console.ReadLine();
+
+
+
+            ////make Point one  
+            //_3DPonit P1 = new _3DPonit();
+            ////make Point Two
+            //_3DPonit P2 = new _3DPonit();
+            //int num = 0 ;
+
+            ////enter x,y,z for P1
+
+            //P1.EnterInput(P1_Input) ;
+
+            ////enter x,y,z for P2
+
+            //P2.EnterInput(P2_Input);
+
+            //// equality of two points
+            //if(P1==P2)
+            //{
+            //    Console.WriteLine("Two points are Equal");
+            //}
+
+            #endregion
+
+            #region Second Project
+
+            //Console.WriteLine( Calculator.Sum(23.3, 32.2));
+            //Console.WriteLine("*************************");
+            //Console.WriteLine( Calculator.Sum(12, 12));
+            //Console.WriteLine("*************************");
+            //Console.WriteLine( Calculator.Mulit(24.2, 4));
+            //Console.WriteLine("*************************");
+            //Console.WriteLine( Calculator.Divide(2, 9));
+            #endregion
+
+            #region Third Project
+
+
+                 Duration D1 =new  Duration (1,10,15);
+            Console.WriteLine( D1.ToString());
+            Console.WriteLine("***************************************");
+                Duration D2 =new Duration (3600);
+            Console.WriteLine( D2.ToString());
+            Console.WriteLine("***************************************");
+                Duration D3 =new Duration (7800);
+            Console.WriteLine(D3.ToString());
+            Console.WriteLine("***************************************");
+                Duration D4 =new Duration (666);
+            Console.WriteLine(D4.ToString());
+            Console.WriteLine("***************************************");
+            //overloading 
+            //1
+            Duration D = D1 + D2;
+            Console.WriteLine( D.ToString());
+            Console.WriteLine("***************************************");
+            //2
+            D = D1 + 7800;
+            Console.WriteLine(D);
+            Console.WriteLine("***************************************");
+            //3
+            D =  666+ D1;
+            Console.WriteLine(D);
+            Console.WriteLine("***************************************");
+            //4
+            D = ++D1;
+            Console.WriteLine(D);
+            Console.WriteLine("***************************************");
+            //5
+            D = --D4;
+            Console.WriteLine(D);
+            Console.WriteLine("***************************************");
+            //6
+            D = D3 - D2;
+            Console.WriteLine(D);
+            Console.WriteLine("***************************************");
+            //7
+            Console.WriteLine((D1 > D2));
+            Console.WriteLine("***************************************");
+            //8
+            Console.WriteLine((D1 <= D2));
+            Console.WriteLine("***************************************");
+            //9
+            DateTime Obj = (DateTime)D1;
+
+
+
+
+
+
+
+
+
+            #endregion
         }
     }
 }
---
+++ Third_project/Duaration.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ConsoleApp1.Third_project
+{
+    internal class Duration
+    {
+       public  int Hours { get; set; }
+       public  int Minutes {  get; set; }
+       public  int Seconds { get; set; }
+
+        public Duration(int hours,int min,int sec)
+        {
+            Hours = hours; Minutes = min; Seconds = sec;
+        }
+        public Duration()
+        {
+            Hours = 0; Minutes = 0; Seconds = 0;
+        }
+        public Duration(int hoursInsecond) { 
+            Hours = hoursInsecond/3600;
+            hoursInsecond%=3600 ;
+            Minutes = hoursInsecond/60;
+            Seconds = hoursInsecond%60;
+        }
+
+
+        static public Duration operator +(Duration left , Duration right)
+        {
+            return new Duration()
+            {
+                Hours = left.Hours + right.Hours,
+                Minutes = left.Minutes + right.Minutes,
+                Seconds = left.Seconds + right.Seconds,
+            };
+        }
+        static public Duration operator +(Duration left,int hoursInsecond)
+        {
+            Duration result = new Duration();
+            result.Hours = left.Hours + (hoursInsecond / 3600);
+            hoursInsecond %= 3600;
+            result. Minutes = left.Minutes + (hoursInsecond / 60);
+            result. Seconds = left.Seconds + (hoursInsecond % 60);
+            return result;
+            
+        }
+
+        static public Duration operator +( int hoursInsecond, Duration left)
+        {
+            Duration result = new Duration();
+            result.Hours = left.Hours + (hoursInsecond / 3600);
+            hoursInsecond %= 3600;
+            result.Minutes = left.Minutes + (hoursInsecond / 60);
+            result.Seconds = left.Seconds + (hoursInsecond % 60);
+            return result;
+
+        }
+
+        static public Duration operator ++(Duration left)
+        {
+
+            return new Duration()
+            {
+                Hours = left.Hours,
+                Minutes = left.Minutes + 1,
+                Seconds = left.Seconds,
+            };
+        }
+        static public Duration operator --(Duration left)
+        {
+
+            return new Duration()
+            {
+                Hours = left.Hours,
+                Minutes = left.Minutes - 1,
+                Seconds = left.Seconds,
+            };
+        }
+        static public Duration operator -(Duration left, Duration right)
+        {
+            return new Duration()
+            {
+                Hours =   (left.Hours - right.Hours)>0?(left.Hours - right.Hours) : 0,
+                Minutes = (left.Minutes - right.Minutes) > 0 ? (left.Minutes - right.Minutes) : 0,
+                Seconds = (left.Seconds - right.Seconds) > 0 ? (left.Seconds - right.Seconds) : 0,
+            };
+        }
+        static public bool operator >(Duration left, Duration right)
+        {
+           if (left.Hours == right.Hours)
+            {
+                if (left.Minutes == right.Minutes)
+                 {
+                    return left.Seconds> right.Seconds;
+                 }
+                else
+                    return left.Minutes > right.Minutes;
+            }
+           else
+                return left.Hours > right.Hours;
+        }
+        static public bool operator <(Duration left, Duration right)
+        {
+            if (left.Hours == right.Hours)
+            {
+                if (left.Minutes == right.Minutes)
+                {
+                    return left.Seconds < right.Seconds;
+                }
+                else
+                    return left.Minutes < right.Minutes;
+            }
+            else
+                return left.Hours < right.Hours;
+        }
+
+        static public bool operator <=(Duration left, Duration right)
+        {
+            if(left.Hours > right.Hours)
+            {
+                if( left.Minutes > right.Minutes)
+                {
+                    return (left.Seconds <= right.Seconds);
+                }
+                return true;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+        static public bool operator >=(Duration left, Duration right)
+        {
+            if (left.Hours < right.Hours)
+            {
+                if (left.Minutes < right.Minutes)
+                {
+                    return (left.Seconds <= right.Seconds);
+                }
+                return true;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        static public explicit operator DateTime(Duration D)
+        {
+            return new DateTime(2025,1,17,D.Hours,D.Minutes,D.Seconds) {
+            };
+            
+        }
+
+        //  Override All System.Object Members(ToString, Equals, GetHasCode) .
+        public override string ToString()
+        {
+            if(Hours != 0) 
+            return$"Hours {Hours} Minutes {Minutes} Seconds {Seconds}";
+            else if(Minutes != 0)
+                return $"Minutes {Minutes} Seconds {Seconds}";
+            else 
+                return $"Seconds {Seconds}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+            else   return true;
+        }
+
+        public override int GetHashCode()
+        {
+            return RuntimeHelpers.GetHashCode(this);
+        }
+    }
+}
